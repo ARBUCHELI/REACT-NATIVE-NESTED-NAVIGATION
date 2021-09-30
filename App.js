d0db5a231a2026a4ba0e3a7d3140eecbacfd91e2
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-09-30 14:51:41 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+// Add the new stack navigator above this line
+const OverviewScreen = () => {
+  return (
+    <View style={styles.layout}>
+      <Text style={styles.title}>Overview</Text>
+    </View>
+  );
+};
+
+export const ProfileNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Overview" component={OverviewScreen} />
+  </Stack.Navigator>
+);
+
+
+const FeedScreen = () => (
+  <View style={styles.layout}>
+    <Text style={styles.title}>Feed</Text>
+  </View>
+);
+
+const Tab = createBottomTabNavigator();
+
+export const AppNavigator = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Feed" component={FeedScreen} />
+    <Tab.Screen name='Profile' component={ProfileNavigator} />
+  </Tab.Navigator>
+);
+
+const App = () => (
+  <NavigationContainer>
+    <AppNavigator />
+  </NavigationContainer>
+);
+
+export default App;
+
+const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 32,
+    marginBottom: 16,
+  },
+});
